906ed4d4aa9b4935142c37e5ce4bf9b6a203aa39
greetjeJongen <greetje.jongen@ucll.be>
2021-10-26 11:31:23 +0200

    opkuis NotAuthorizedException

--- src/main/java/ui/controller/NotAuthorizedException.java
+++ src/main/java/ui/controller/NotAuthorizedException.java
@@ -1,10 +1,6 @@
 package ui.controller;
 
 public class NotAuthorizedException extends RuntimeException {
- /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
 public NotAuthorizedException() {
 	 super();
